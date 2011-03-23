4b03da86d754d18bdbf0dec2e6393db308a19fbb
root <root@GuayoLap.(none)>
2011-03-22 19:19:09 -0600

    las formas

---
+++ lista.php
@@ -0,0 +1,52 @@
+<html>
+	<head>
+		<title>
+			Lab 3 Arqui
+		</title>
+	</head>
+	
+	<body>
+	
+		<h2>Lista de Usuarios</h2>
+		
+		<table border="1">
+			<tr>
+				<th>
+					Matricula
+				</th>
+				<th>
+					Nombre
+				</th>
+				<th>
+					Apellidos
+				</th>
+				<th>
+					Email
+				</th>
+			<tr/>
+
+			<!--Aqui va el for-->
+			<tr>
+				<td>
+					Matricula!!
+				</td>
+				<td>
+					Nombre!!
+				</td>
+				<td>
+					Apellidos!!
+				</td>
+				<td>
+					Email!!
+				</td>
+				
+			</tr>
+			<!--Termina el for-->
+				
+		
+		</table>
+		
+		
+	</body>
+
+</html>
